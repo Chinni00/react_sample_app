7adc9aa338650aa1a70646f56fc65371f84e5d1c
chinni00 <chinni0437@gmail>
2023-05-17 12:15:55 +0530

    added delete buttton

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,18 +1,23 @@
 import React from 'react'
 
 const Data=(props)=> {
+
     const {expenses} =props
+    const eventHandler = ()=>{
+      console.log('clicked!!!')
+    }
+
   return (
     <div>
         {expenses.map((expense)=>{ return [
-    <div className="expense-item">
-    <div key={expense.id}>{expense.date.toISOString()}</div>
+    <div key={expense.id} className="expense-item">
+    <div className="expense-item__price">{expense.date.toDateString()}</div>
     <div className="expense-item__description">
       <h2 >{expense.title}</h2>
       <h2>{expense.location}</h2>
       <div className="expense-item__price">${expense.price}</div>
     </div>
-
+    <button onClick={eventHandler}>Delete</button>
    </div>
    ]})}
     </div>
